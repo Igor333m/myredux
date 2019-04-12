482e2b10ea9f99fb4fb92293fef904aae735de7f
Igor Todorovic <igor333m@yahoo.com>
2019-04-11 22:29:07 -0700

    Comment old vanilla js code

--- app.js
+++ app.js
@@ -209,80 +209,80 @@ store.subscribe(() => {
   console.log(store.getState());
   const { goals, todos } = store.getState();
 
-  document.getElementById('goals').innerHTML = '';
-  document.getElementById('todos').innerHTML = '';
+  // document.getElementById('goals').innerHTML = '';
+  // document.getElementById('todos').innerHTML = '';
 
-  goals.forEach(addGoalToDOM);
-  todos.forEach(addTodoToDOM);
+  // goals.forEach(addGoalToDOM);
+  // todos.forEach(addTodoToDOM);
 });
 
-// DOM code
+// DOM code for vanilla js
 
-function addTodo () {
-  const input = document.getElementById('todo');
-  const name = input.value;
-  input.value = '';
+// function addTodo () {
+//   const input = document.getElementById('todo');
+//   const name = input.value;
+//   input.value = '';
 
-  store.dispatch (addTodoAction({
-    name,
-    complete: false,
-    id: generateId()
-  }));
-}
+//   store.dispatch (addTodoAction({
+//     name,
+//     complete: false,
+//     id: generateId()
+//   }));
+// }
 
-function addGoal () {
-  const input = document.getElementById('goal');
-  const name = input.value;
-  input.value = '';
+// function addGoal () {
+//   const input = document.getElementById('goal');
+//   const name = input.value;
+//   input.value = '';
 
-  store.dispatch(addGoalAction({
-    name,
-    id: generateId()
-  }));
-}
+//   store.dispatch(addGoalAction({
+//     name,
+//     id: generateId()
+//   }));
+// }
 
-document.getElementById('todoBtn')
-  .addEventListener('click', addTodo);
+// document.getElementById('todoBtn')
+//   .addEventListener('click', addTodo);
 
-document.getElementById('goalBtn')
-  .addEventListener('click', addGoal);
+// document.getElementById('goalBtn')
+//   .addEventListener('click', addGoal);
 
-function createRemoveButton (onClick) {
-  const removeBtn = document.createElement('button');
-  removeBtn.innerHTML = 'X';
-  removeBtn.addEventListener('click', onClick);
-  return removeBtn;
-}
+// function createRemoveButton (onClick) {
+//   const removeBtn = document.createElement('button');
+//   removeBtn.innerHTML = 'X';
+//   removeBtn.addEventListener('click', onClick);
+//   return removeBtn;
+// }
 
-function addTodoToDOM (todo) {
-  const node = document.createElement('li');
-  const text = document.createTextNode(todo.name);
+// function addTodoToDOM (todo) {
+//   const node = document.createElement('li');
+//   const text = document.createTextNode(todo.name);
 
-  const removeBtn = createRemoveButton( () => {
-    store.dispatch(removeTodoAction(todo.id));
-  });
+//   const removeBtn = createRemoveButton( () => {
+//     store.dispatch(removeTodoAction(todo.id));
+//   });
 
-  node.appendChild(text);
-  node.appendChild(removeBtn);
-  node.style.textDecoration = todo.complete ? 'line-through' : 'none';
-  node.addEventListener('click', () => {
-    store.dispatch(toggleTodoAction(todo.id));
-  });
+//   node.appendChild(text);
+//   node.appendChild(removeBtn);
+//   node.style.textDecoration = todo.complete ? 'line-through' : 'none';
+//   node.addEventListener('click', () => {
+//     store.dispatch(toggleTodoAction(todo.id));
+//   });
 
-  document.getElementById('todos').appendChild(node);
-}
+//   document.getElementById('todos').appendChild(node);
+// }
 
-function addGoalToDOM (goal) {
-  const node = document.createElement('li');
-  const text = document.createTextNode(goal.name);
+// function addGoalToDOM (goal) {
+//   const node = document.createElement('li');
+//   const text = document.createTextNode(goal.name);
 
-  const removeBtn = createRemoveButton( () => {
-    store.dispatch(removeGoalAction(goal.id));
-  });
+//   const removeBtn = createRemoveButton( () => {
+//     store.dispatch(removeGoalAction(goal.id));
+//   });
 
-  node.appendChild(text);
-  node.appendChild(removeBtn);
-  node.style.textDecoration = todo.complete ? 'line-through' : 'none';
+//   node.appendChild(text);
+//   node.appendChild(removeBtn);
+//   node.style.textDecoration = todo.complete ? 'line-through' : 'none';
   
-  document.getElementById('goals').appendChild(node);
-}
+//   document.getElementById('goals').appendChild(node);
+// }
