e49a8954e569cb84c7658b9667d4fafa986d3cd4
Igor Todorovic <igor333m@yahoo.com>
2019-04-28 14:33:34 -0700

    Migrate createStore and Provider to main index.js file

--- todo-app-redux-react/src/index.js
+++ todo-app-redux-react/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import reducer from './reducers';
+import middleWare from './middleware';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore (reducer, middleWare);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
